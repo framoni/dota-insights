125cbcc95aa1f768a8bbd72f88b31d0b007b7e0a
Francesco Ramoni <francesco.ramoni@finleap.com>
2021-08-24 12:57:02 +0200

    Get data for mid W/L analysis

--- api.py
+++ api.py
@@ -5,25 +5,19 @@ https://github.com/odota/web/blob/master/src/lang/en-US.json
 
 """
 
-'''
-
-* fetch public matches starting from a fixed date, within a certain MMR range, game mode Ranked and lobby type All Draft
-* get data about found matches
-* if exposed player is found with lane_role = 2
-    * get info about his N most recent matches
-    * if the N matches are all Ranked and All Draft, return win rate
-
-'''
-
+import csv
 import json
 import requests
 
 MID_ROLE = 2
+LOBBY_ALL_DRAFT = 7
+GAME_MODE_RANKED = 22
+NUM_MMR_LOW_SKILL = 2
 
 pm_url = "https://api.opendota.com/api/explorer?sql=SELECT * \
     FROM public_matches \
-    WHERE public_matches.start_time >= extract(epoch from timestamp '2021-08-21T00:00:00.000Z') \
-    AND public_matches.lobby_type = 7 AND public_matches.game_mode = 22 AND public_matches.num_mmr = 2"
+    WHERE public_matches.start_time >= extract(epoch from timestamp {}) \
+    AND public_matches.lobby_type = {} AND public_matches.game_mode = {} AND public_matches.num_mmr = {}"
 
 match_url = "https://api.opendota.com/api/matches/{}"
 
@@ -33,9 +27,9 @@ payload = {}
 headers = {}
 
 
-def get_mid_players():
+def get_mid_players(timestamp):
     mid_players = []
-    pm = requests.request("GET", pm_url, headers=headers, data=payload)
+    pm = requests.request("GET", pm_url.format(timestamp, LOBBY_ALL_DRAFT, GAME_MODE_RANKED, NUM_MMR_LOW_SKILL), headers=headers, data=payload)
     pm = json.loads(pm.text)['rows']
     for it in pm:
         match = requests.request("GET", match_url.format(it['match_id']), headers=headers, data=payload)
@@ -61,5 +55,20 @@ def get_wl(player_id, n_recent=10):
 
 
 if __name__ == '__main__':
-    print(get_mid_players())
-    get_wl(295280530, n_recent=10)
+
+    '''
+    * fetch public matches starting from a fixed date, within a certain MMR range, game mode Ranked and lobby type All Draft
+    * get data about found matches
+    * if exposed player is found with lane_role = 2
+        * get info about his N most recent matches
+        * if the N matches are all Ranked and All Draft, return win rate
+    '''
+    n_recent = 10
+    from_date = '2021-08-21T00:00:00.000Z'
+    wl = []
+    players = get_mid_players(from_date)
+    for it in players:
+        wl.append(get_wl(it, n_recent))
+    with open('wl_n_{}_from_{}.csv'.format(n_recent, from_date), 'w', newline='') as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+        wr.writerow(wl)
