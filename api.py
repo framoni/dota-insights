f61b5fcbe12b308d64d9acb8e86f5a55e843c52b
Francesco Ramoni <francesco.ramoni@finleap.com>
2021-09-25 01:47:20 +0200

    Fix KeyError, add functionality for resuming

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ https://github.com/odota/web/blob/master/src/lang/en-US.json
 """
 
 import json
+import os
 import pandas as pd
 import requests
 import time
@@ -31,20 +32,31 @@ payload = {}
 headers = {}
 
 
-def get_mid_players(timestamp, n_recent, limit):
-    mid_players_df = pd.DataFrame(columns=['match_id', 'player_id', 'win_rate'])
-    mid_players = []
-    pm = requests.request("GET", pm_url.format(timestamp, LOBBY_ALL_DRAFT, GAME_MODE_RANKED, NUM_MMR_LOW_SKILL),
-                          headers=headers, data=payload)
-    pm = json.loads(pm.text)['rows']
-    with open('pm_from_{}.json'.format(timestamp), 'w') as f:
-        f.write(json.dumps(pm))
+def get_mid_players(timestamp, n_recent, limit, start):
+    if os.path.isfile('wl_n_{}_from_{}.csv'.format(n_recent, timestamp)):
+        mid_players_df = pd.read_csv('wl_n_{}_from_{}.csv'.format(n_recent, timestamp))
+        mid_players = list(mid_players_df['player_id'].astype(int))
+    else:
+        mid_players_df = pd.DataFrame(columns=['match_id', 'player_id', 'win_rate'])
+        mid_players = []
+    if os.path.isfile('pm_from_{}.json'.format(timestamp)):
+        pm = json.loads(open('pm_from_{}.json'.format(timestamp), 'r').read())
+    else:
+        pm = requests.request("GET", pm_url.format(timestamp, LOBBY_ALL_DRAFT, GAME_MODE_RANKED, NUM_MMR_LOW_SKILL),
+                              headers=headers, data=payload)
+        pm = json.loads(pm.text)['rows']
+        with open('pm_from_{}.json'.format(timestamp), 'w') as f:
+            f.write(json.dumps(pm))
+    counter = 0
     for it in tqdm(pm):
+        counter += 1
+        if counter < start:
+            continue
         match = requests.request("GET", match_url.format(it['match_id']), headers=headers, data=payload)
         time.sleep(1)
         match = json.loads(match.text)
-        for p in match['players']:
-            try:
+        try:
+            for p in match['players']:
                 if p['lane_role'] == MID_ROLE and p['account_id'] is not None \
                         and p['account_id'] not in mid_players and is_bad(p):
                     wl = get_wl(p['account_id'], it['match_id'], n_recent)
@@ -57,9 +69,8 @@ def get_mid_players(timestamp, n_recent, limit):
                         if len(mid_players) == limit:
                             print('Limit of found players is reached')
                             return
-                    break
-            except KeyError:
-                continue
+        except KeyError:
+            continue
     mid_players_df.to_csv('wl_n_{}_from_{}.csv'.format(n_recent, timestamp), index=False)
 
 
@@ -101,5 +112,5 @@ if __name__ == '__main__':
     from_date = '2021-08-21T00:00:00.000Z'
     # from_date = '2021-09-01T00:00:00.000Z'
 
-    get_mid_players(from_date, n_recent, limit=500)
+    get_mid_players(from_date, n_recent, limit=500, start=7008)
 
